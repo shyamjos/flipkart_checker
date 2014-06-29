70705d110e489213983b08b1af2cdeed73837265
shyam jos <shyamjosepp@gmail.com>
2014-06-29 22:18:09 +0530

    final touches

--- flipkart.py
+++ flipkart.py
@@ -1,13 +1,13 @@
 import urllib2
 import re
 import subprocess
-flipkart_url = 'copy flipkart product url to monitor here'
+flipkart_url = 'http://www.flipkart.com/moto-e/p/itmdvuwsybgnbtha?pid=MOBDVHC6XKKPZ3GZ&otracker=ts_motoe_black'
 flipkart_fetch = urllib2.urlopen(flipkart_url).read()
 
 product = re.findall('Notify me when this product is in stock', flipkart_fetch);
 
 if len(product) == 0: 
-   message = "your product is  back in stock at Flipkart "
+   message = "Moto E back in stock at Flipkart "
    subprocess.Popen(['notify-send', message])
 else:
    print 'Your product is not avialble :('
