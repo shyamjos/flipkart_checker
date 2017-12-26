ddcfd0634ee0df78641d9c0319388ba5285ea4fd
Sourabh Saini <sourabh.saini08@gmail.com>
2017-12-26 17:44:02 +0530

    Update flipkart_checker.py

--- flipkart_checker.py
+++ flipkart_checker.py
@@ -2,14 +2,13 @@ import urllib2
 import re
 import subprocess
 # URL to the product
-flipkart_url = 'URL to product'
+flipkart_url = '<ENTER THE URL HERE>'
 flipkart_fetch = urllib2.urlopen(flipkart_url).read()
-# the magic line (change this accordingly :) )
-product = re.findall('Notify me when this product is in stock', flipkart_fetch);
-# message to print 
+
+product = re.findall('This item is currently out of stock', flipkart_fetch);
+
 if len(product) == 0: 
-   message = "your product is back in stock at Flipkart "
-   subprocess.Popen(['notify-send', message])
+	message = "The Product is back in stock at Flipkart "
 else:
-# this line only prints in terminal
-   print 'Your product is not available '
+	message = "Product is not available yet'
+subprocess.Popen(['notify-send', message])
